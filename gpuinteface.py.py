a9154b6e9851effeaa658ad989399b67a661f9b3
22141bhs <22141@burnside.school.nz>
2024-05-24 10:57:53 +1200

    added basic user interface

--- gpuinteface.py.py
+++ gpuinteface.py.py
@@ -1,23 +1,23 @@
 import sqlite3
 from colorama import Fore, Back, Style
-
+query = 'SELECT gpu.name, manufacturer.name, gpu.price, gpu.speed FROM gpu JOIN manufacturer ON gpu.manufacturer_id = manufacturer.id'
 print(Fore.BLUE + "="*42)
 print(Fore.GREEN + "         Welcome to GPU data base")
 print(Fore.BLUE + "=" *42  + Style.RESET_ALL)
-
 def print_gpu():
-    maker = input("makier??????")
-    price = input("pricE?????")
     with sqlite3.connect('gpu.db') as db:
         cursor = db.cursor()
-        sql = f"SELECT gpu.name, manufacturer.name, gpu.price, gpu.speed FROM gpu JOIN manufacturer ON gpu.manufacturer_id = manufacturer.id WHERE manufacturer_id = {maker} AND price < {price};"
+        sql = f"{query};"
         cursor.execute(sql)
         results = cursor.fetchall()
         print(f"{"Name":<20}{"Manufacturer":<15}{"Price ($)":<10}{"Speed (MHz)":<14}")
         for gpu in results:
             print(f"{gpu[0]:<20}{gpu[1]:<15}{gpu[2]:<10}{gpu[3]:<14}")
 
-    
-
-print_gpu()
+while True:
+    ask = input("What would you like to do\n> 1. Print all Data\n> 4. Exit\n")
+    if ask == "1":
+        print_gpu()
+    if ask == "4":
+        exit()
         
