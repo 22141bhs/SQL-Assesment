6564ee20a1bd8187728451321d6c9a89163bf755
22141bhs <22141@burnside.school.nz>
2024-05-30 15:06:41 +1200

    added some quality of life chaneges like adding a dollar sign whn asking for price and adding a press eneter to contine afer it displays info solet u read it

--- gpuinteface.py.py
+++ gpuinteface.py.py
@@ -3,7 +3,7 @@ from colorama import Fore, Back, Style
 
 def print_header():
     print(Fore.BLUE + "="*42)
-    print(Fore.GREEN + "         Welcome to GPU data base")
+    print(Fore.GREEN + "         Welcome to GPU Database")
     print(Fore.BLUE + "=" * 42 + Style.RESET_ALL)
 
 def print_gpu():
@@ -19,8 +19,8 @@ def print_gpu():
 def print_gpu_ask():
     with sqlite3.connect('gpu.db') as db:
         maker = input("Please input a manufacturer\n> 1. AMD\n> 2. Nvidia\n> ")
-        price = input("Please input the upper end of your price range\n> ")
-        speed = input("Please input the upper end of VRAM required (in MB)\n> ")
+        price = input("Please input the upper end of your price range\n$ ")
+        speed = input("Please input the upper end of VRAM required (in MB)(1024 = 1GB)\n> ")
         cursor = db.cursor()
         sql = f"SELECT gpu.name, manufacturer.name, gpu.price, gpu.speed FROM gpu JOIN manufacturer ON gpu.manufacturer_id = manufacturer.id WHERE manufacturer_id = {maker} AND price < {price} AND speed < {speed};"
         cursor.execute(sql)
@@ -33,7 +33,7 @@ def add_gpu():
     with sqlite3.connect('gpu.db') as db:
         name = input("Enter GPU name: ")
         manufacturer_id = input("Enter manufacturer ID (1 for AMD, 2 for Nvidia): ")
-        price = input("Enter GPU price: ")
+        price = input("Enter GPU price: $")
         speed = input("Enter GPU VRAM (in MB): ")
         cursor = db.cursor()
         sql = "INSERT INTO gpu (name, manufacturer_id, price, speed) VALUES (?, ?, ?, ?);"
@@ -47,9 +47,12 @@ while True:
     ask = input("What would you like to do\n> 1. Print all Data\n> 2. Search for parts\n> 3. Add a GPU\n> 4. Exit\n> ")
     if ask == "1":
         print_gpu()
+        input("Press enter to continue ")
     elif ask == "2":
         print_gpu_ask()
+        input("Press enter to continue ")
     elif ask == "3":
         add_gpu()
+        input("Press enter to continue ")
     elif ask == "4":
         exit()
